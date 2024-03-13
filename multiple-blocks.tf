36959f645038ad92185aca2abe13b129e80510ba
suyash460 <71191009+suyash460@users.noreply.github.com>
2024-03-13 23:55:04 +0530

    Update multiple-blocks.tf

--- multiple-blocks.tf
+++ multiple-blocks.tf
@@ -1,7 +1,7 @@
-# output first-block{
-#     value = "This is 1st block"
-# }
+output first-block{
+    value = "This is 1st block"
+}
 
-# output second-block{
-#     value = "This is 2nd block"
-# }
+output second-block{
+    value = "This is 2nd block"
+}
